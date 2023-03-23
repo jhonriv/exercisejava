26779f277b998deb726c25381cb9a87497ee4953
JhonRiv <jhrivero29@gmail.com>
2023-03-22 20:40:42 -0400

    first commit

---
+++ src/main/java/com/jhonriv/exercisejava/ExerciseJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jhonriv.exercisejava;
+
+/**
+ *
+ * @author JhonRiv
+ */
+public class ExerciseJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
